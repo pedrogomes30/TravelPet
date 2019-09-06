b33d139a87d13d27c40fb44f4c05763eac9c9194
rodrigo <rodrigo.de.cf@gmail.com>
2019-09-06 00:01:05 -0300

    Commit conexão Firebase

--- app/src/main/java/com/example/travelpet/MainActivity.java
+++ app/src/main/java/com/example/travelpet/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.travelpet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth fbAuth;
+    FirebaseDatabase fbDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fbAuth = FirebaseAuth.getInstance();
+        fbDB = FirebaseDatabase.getInstance();
+
+
+    }
+
+
+
+
+    public void ToastThis (String mensagem)
+    {
+        Toast.makeText(this, mensagem,Toast.LENGTH_LONG).show();
+        //Mostra algo na tela, usado para testes
     }
 }
